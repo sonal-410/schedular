ec54c4664925c964af5d76b27b608d215d5138eb
Kamlesh Gupta <kamleshgupta@Kamleshs-MacBook-Pro.local>
2019-01-14 16:00:14 +0530

    schedule create

--- schedule.go
+++ schedule.go
@@ -2,27 +2,42 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
 type scheduler struct {
-	TaskId    int   `json:"taskId"`
+	TaskID    int   `json:"taskId"`
 	TimeStamp int64 `json:"timestamp"`
 	Flag      bool  `json:"flag"`
 }
 
 var db *sql.DB
 
-func schedule(w http.ResponseWriter, r *http.Request){
-	
+func schedule(w http.ResponseWriter, r *http.Request) {
+	var q scheduler
+	_ = json.NewDecoder(r.Body).Decode(&q)
+	fmt.Println(q)
+	stmtIns, er := db.Prepare("insert into scheduler values(?,?,?)")
+	if er != nil {
+		panic(er.Error())
+	} else {
+		fmt.Println("entity add")
+	}
+	_, err := stmtIns.Exec(q.TaskID, q.TimeStamp, q.Flag)
+	if err != nil {
+		panic(err.Error())
+	}
+
 }
 
 func main() {
 	r := mux.NewRouter()
 	db, _ = sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/todo")
 	defer db.Close()
-	r.HandleFunc("/schedule/{id:[0-9]+}", schedule)
+	r.HandleFunc("/schedule/{id:[0-9]+}", schedule).Methods("GET")
 	http.ListenAndServe(":8000", r)
 }
